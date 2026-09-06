ba98329db503425ace558a42d5ff549e830dde29
aidandmello-rgb <aidan.dmello@gmail.com>
2026-09-06 11:17:04 +0530

    print and comments

--- A.I. Expert/main.py
+++ A.I. Expert/main.py
@@ -1,1 +1,8 @@
 print("helloworld")
+#printing a greeting to the new world
+print("My name is Aidan")
+#printing my name
+print("I play volleyball")
+#printing my hobby
+print("6+8 =", 6+8)
+#simple calculation
