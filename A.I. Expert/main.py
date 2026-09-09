392f29e010438358e8bda3f4360847e5d7536345
aidandmello-rgb <aidan.dmello@gmail.com>
2026-09-09 14:37:01 +0530

    homework commit

--- A.I. Expert/main.py
+++ A.I. Expert/main.py
@@ -1,8 +1,23 @@
-print("helloworld")
-#printing a greeting to the new world
-print("My name is Aidan")
-#printing my name
-print("I play volleyball")
-#printing my hobby
-print("6+8 =", 6+8)
-#simple calculation
+#waking at a certain time
+print("I wake up at 6:00 am")
+
+#preparing for the day and leaving fo school
+print("I get dressed and have breakfast and leave for school at 7:00 am")
+
+#eating lunch and watching tv post school
+print("I eat lunch and watch TV after school at 2:00 pm")
+
+#playing video games and doing homework
+print("I play video games and do my homework at 3:00 pm")
+
+#after homework, studying
+print("After homework, I study for my exams at 3:30 pm")
+
+#amount of study time in a week
+print("I study for 2.5 hours a day that is", 7 * 2.5, "hours a week")
+
+#going to the gym and eating dinner
+print("I go to the gym and eat dinner at 6:00 pm")  
+
+#going to bed
+print("I go to bed at 10:00 pm")
