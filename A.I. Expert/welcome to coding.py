96d423c8846c4270aa8dfaf21e0d1c1452b575ce
aidandmello-rgb <aidan.dmello@gmail.com>
2026-09-12 11:04:07 +0530

    welcome to codingal

---
+++ A.I. Expert/welcome to coding.py
@@ -0,0 +1,13 @@
+print("Welcome to the world of coding!")
+#this is a welcome greet
+
+print(1)
+#printing a number
+
+print("Hello \n World")
+#printing a string with a new line character
+
+print("Hello \t World")
+#printing a string with a tab space character
+
+print("Hello World", end=".")
