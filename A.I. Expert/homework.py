cf99e9c1ad13fef51df1e9120bc7f758ac2b60ca
aidandmello-rgb <aidan.dmello@gmail.com>
2026-09-20 11:30:05 +0530

    homework

---
+++ A.I. Expert/homework.py
@@ -0,0 +1,20 @@
+aidan = "15 September 2014"
+#aidans birthday
+
+ali = "24 September 2014"
+#alis birthday
+
+hiranya = "23 November 2013"
+#hiranyas birthday
+
+vandit = "1 July 2014"
+#vandits birthday
+
+devansh = "12 January 2014"
+#devanshs birthday
+
+print("aidans birthday is", aidan)
+print("alis birthday is", ali)
+print("hiranyas birthday is", hiranya)
+print("vandits birthday is", vandit)
+print("devanshs birthday is", devansh)
